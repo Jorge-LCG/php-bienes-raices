b6fd039fe955202e01fef5bca71b29e170f79db9
Jorge-LCG <jorgeluiscg2003@gmail.com>
2025-12-16 17:03:59 -0500

    Se agrego dark mode

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
     eventListerners();
+    darkMode();
 });
 
+function darkMode() {
+    const darkModeBoton = document.querySelector(".dark-mode-boton");
+    const isDarkMode = localStorage.getItem("darkMode");
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+    if (prefiereDarkMode.matches) {
+        document.body.classList.add("dark-mode");
+    } else {
+        document.body.classList.remove("dark-mode");
+    }
+
+    prefiereDarkMode.addEventListener("change", function () {
+        if (prefiereDarkMode.matches) {
+            document.body.classList.add("dark-mode");
+        } else {
+            document.body.classList.remove("dark-mode");
+        }
+    });
+
+    if (isDarkMode === "enabled") {
+        document.body.classList.add('dark-mode');
+    }
+
+    darkModeBoton.addEventListener("click", function () {
+        document.body.classList.toggle('dark-mode');
+ 
+        if (document.body.classList.contains('dark-mode')) {
+            localStorage.setItem('darkMode', 'enabled');
+        } else {
+            localStorage.setItem('darkMode', 'disabled');
+        }
+    });
+}
+
 function eventListerners() {
-    document.addEventListener("click", navegacionResponsiva);
+    const mobileBoton = document.querySelector(".mobile-menu");
+    mobileBoton.addEventListener("click", navegacionResponsiva);
 }
 
 function navegacionResponsiva() {
