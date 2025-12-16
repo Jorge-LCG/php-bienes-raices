94568ca2f32db6c6b6ed5fdf44a517cd9824f546
Jorge-LCG <jorgeluiscg2003@gmail.com>
2025-12-16 17:34:56 -0500

    Creación de footer con PHP

--- contacto.php
+++ contacto.php
@@ -66,19 +66,4 @@
         </form>
     </main>
 
-    <footer class="footer">
-        <div class="contenedor contenedor-footer">
-            <nav class="navegacion">
-                <a href="nosotros.php">Nosotros</a>
-                <a href="anuncios.php">Anuncios</a>
-                <a href="blog.php">Blog</a>
-                <a href="contacto.php">Contacto</a>
-            </nav>
-
-            <p class="copyright">Todos los derechos reservados 2025 &copy;</p>
-        </div>
-    </footer>
-
-    <script src="build/js/app.min.js"></script>
-</body>
-</html>
+<?php include "includes/templates/footer.php"; ?>
---
+++ includes/templates/footer.php
@@ -0,0 +1,16 @@
+    <footer class="footer">
+        <div class="contenedor contenedor-footer">
+            <nav class="navegacion">
+                <a href="nosotros.php">Nosotros</a>
+                <a href="anuncios.php">Anuncios</a>
+                <a href="blog.php">Blog</a>
+                <a href="contacto.php">Contacto</a>
+            </nav>
+
+            <p class="copyright">Todos los derechos reservados <?php echo date('Y'); ?> &copy;</p>
+        </div>
+    </footer>
+
+    <script src="build/js/app.min.js"></script>
+</body>
+</html>
