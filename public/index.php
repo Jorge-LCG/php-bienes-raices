d9612f2ea8a29e088a8f89b11b8809b843e9ebc7
Jorge-LCG <jorgeluiscg2003@gmail.com>
2025-12-24 00:52:13 -0500

    Registrando URLS a métodos asociados del controlador

--- public/index.php
+++ public/index.php
@@ -1,3 +1,12 @@
 <?php
 
 require_once __DIR__ . "/../includes/app.php";
+
+use MVC\Router;
+
+$router = new Router();
+
+$router->get("/nosotros", 'funcion_nosotros');
+$router->get("/contactanos", 'funcion_contactanos');
+
+$router->comprobarRutas();
