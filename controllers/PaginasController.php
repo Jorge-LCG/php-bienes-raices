306883abb9b3b73c1f4e2edd954ac410c7ca0139
Jorge-LCG <jorgeluiscg2003@gmail.com>
2025-12-24 19:53:59 -0500

    Creando URL y controlador de página principal

---
+++ controllers/PaginasController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Controllers;
+
+class PaginasController {
+    public static function index() {
+        echo "index página principal";
+    }
+
+    public static function nosotros() {
+        echo "nosotros página principal";
+    }
+
+    public static function propiedades() {
+        echo "propiedades página principal";
+    }
+
+    public static function propiedad() {
+        echo "propiedad página principal";
+    }
+
+    public static function blog() {
+        echo "blog página principal";
+    }
+
+    public static function entrada() {
+        echo "entrada página principal";
+    }
+
+    public static function contacto() {
+        echo "contacto página principal";
+    }
+}
